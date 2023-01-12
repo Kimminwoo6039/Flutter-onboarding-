b9b6c58c94c1ff12d84265b1356645ae17ad2d68
kimminwoo6039 <qkqhemforhs1@gmail.com>
2023-01-12 20:30:29 +0900

    onboarding 기본 소스

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: onboardingPage(),
+    );
+  }
+}
+
+class onboardingPage extends StatelessWidget {
+  const onboardingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IntroductionScreen(
+        pages: [
+          // 페이지1
+          PageViewModel(
+            title: "Title1",
+            bodyWidget: Text("body1"),
+          ),
+
+          // 페이지2
+          PageViewModel(
+            title: "Title2",
+            bodyWidget: Text("body2"),
+          ),
+        ],
+        next: Text("Next", style: TextStyle(fontWeight: FontWeight.w600)),
+        done: Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
+        onDone: () {
+          // When done button is press
+        },
+      ),
+    );
+  }
+}
